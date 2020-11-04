2503efdc2892c33a4f24d7d746d2697c5a5b5967
Mike Wallio <michael.wallio@outlook.com>
2020-11-04 14:34:32 -0500

    Ignore whitespace in search regex

--- src/utils/regex.cpp
+++ src/utils/regex.cpp
@@ -17,6 +17,11 @@ namespace tofi
         std::wstring build_pattern(std::wstring_view search)
         {
             std::wstring pattern{std::accumulate(std::begin(search), std::end(search), std::wstring{}, [](std::wstring sum, wchar_t ch) {
+                if (std::isspace(ch))
+                {
+                    return sum;
+                }
+
                 return std::move(sum) +
                        (std::any_of(SPECIAL_CHARS, SPECIAL_CHARS + wcslen(SPECIAL_CHARS), std::bind(std::equal_to<wchar_t>(), std::placeholders::_1, ch)) ? L"\\" : L"") +
                        ch +
